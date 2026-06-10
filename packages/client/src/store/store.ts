b6e284852a56183db1f57dc4fc9ff0d855b84c15
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2026-06-10 21:11:33 +0530

    add store

---
+++ packages/client/src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { rootReducer } from './rootReducer';
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: import.meta.env.MODE !== 'production',
+});
+
+export type AppDispatch = typeof store.dispatch;
