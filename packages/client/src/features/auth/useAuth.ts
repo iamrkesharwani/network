e87e7c59a0e14c26d39bc4be30c4bbc6b6471686
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2026-06-26 15:29:55 +0530

    feat: add authentication hook

---
+++ packages/client/src/features/auth/useAuth.ts
@@ -0,0 +1,9 @@
+import { useAppSelector } from '../../shared/hooks/useAppSelector';
+
+export const useAuth = () => {
+  const { user, accessToken, isAuthenticated, isInitialized } = useAppSelector(
+    (state) => state.auth
+  );
+
+  return { user, accessToken, isAuthenticated, isInitialized };
+};
