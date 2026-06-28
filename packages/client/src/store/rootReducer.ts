d3ae75b311076a0b2e09260402f1aaeb8a31a9bb
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2026-06-28 18:01:01 +0530

    fix: store fix

--- packages/client/src/store/rootReducer.ts
+++ packages/client/src/store/rootReducer.ts
@@ -2,15 +2,19 @@ import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import uiReducer from '../features/ui/uiSlice';
 import videoReducer from '../features/video/videoSlice';
+import shortReducer from '../features/short/shortSlice';
 import { authApi } from '../features/auth/authApi';
 import { videoApi } from '../features/video/videoApi';
+import { shortApi } from '../features/short/shortApi';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   ui: uiReducer,
   video: videoReducer,
+  short: shortReducer,
   [authApi.reducerPath]: authApi.reducer,
   [videoApi.reducerPath]: videoApi.reducer,
+  [shortApi.reducerPath]: shortApi.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -2,13 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import rootReducer, { type RootState as RootReducerState } from './rootReducer';
 import { authApi } from '../features/auth/authApi';
 import { videoApi } from '../features/video/videoApi';
+import { shortApi } from '../features/short/shortApi';
 
 export const createAppStore = (preloadedState?: Partial<RootReducerState>) =>
   configureStore({
     reducer: rootReducer,
     preloadedState,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(authApi.middleware, videoApi.middleware),
+      getDefaultMiddleware().concat(
+        authApi.middleware,
+        videoApi.middleware,
+        shortApi.middleware
+      ),
     devTools: import.meta.env.MODE !== 'production',
   });
 
