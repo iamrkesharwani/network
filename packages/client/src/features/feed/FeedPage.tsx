eedaa9f2111bb5fb0ed8ba5514c8cf6e74379284
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2026-06-29 00:17:05 +0530

    feat: add feed

--- packages/client/src/features/feed/FeedPage.tsx
+++ packages/client/src/features/feed/FeedPage.tsx
@@ -1,25 +1,39 @@
-import FeedDesktop from './FeedDesktop';
-import FeedTablet from './FeedTablet';
-import FeedMobile from './FeedMobile';
 import usePageTitle from '../../shared/hooks/usePageTitle';
+import { useFeedLayout } from './hooks/useFeedLayout';
+import FeedDesktop from './desktop/FeedDesktop';
+import FeedTabletLandscape from './tablet/FeedTabletLandscape';
+import FeedTabletPortrait from './tablet/FeedTabletPortrait';
+import FeedMobilePortrait from './mobile/FeedMobilePortrait';
+import FeedMobileLandscape from './mobile/FeedMobileLandscape';
 
 const FeedPage = () => {
   usePageTitle('Feed');
+  const { tier, orientation } = useFeedLayout();
 
-  return (
-    <>
-      <div className="md:hidden">
-        <FeedMobile />
-      </div>
-
-      <div className="hidden md:block xl:hidden -mx-4 -mt-4 sm:-mx-6 sm:-mt-6 lg:-mx-8 lg:-mt-8">
-        <FeedTablet />
-      </div>
-
-      <div className="hidden xl:block -mx-4 -mt-4 sm:-mx-6 sm:-mt-6 lg:-mx-8 lg:-mt-8">
+  if (tier === 'desktop') {
+    return (
+      <div className="-mx-4 -mt-4 sm:-mx-6 sm:-mt-6 lg:-mx-8 lg:-mt-8">
         <FeedDesktop />
       </div>
-    </>
+    );
+  }
+
+  if (tier === 'tablet') {
+    return (
+      <div className="-mx-4 -mt-4 sm:-mx-6 sm:-mt-6 lg:-mx-8 lg:-mt-8">
+        {orientation === 'landscape' ? (
+          <FeedTabletLandscape />
+        ) : (
+          <FeedTabletPortrait />
+        )}
+      </div>
+    );
+  }
+
+  return orientation === 'landscape' ? (
+    <FeedMobileLandscape />
+  ) : (
+    <FeedMobilePortrait />
   );
 };
 
